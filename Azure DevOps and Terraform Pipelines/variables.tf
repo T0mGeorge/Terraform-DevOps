fd74c2214412397876d6c869b8fda2d491b36bb6
Tom George <92213820+T0mGeorge@users.noreply.github.com>
2022-09-12 18:30:50 +0100

    Update variables.tf

--- Azure DevOps and Terraform Pipelines/variables.tf
+++ Azure DevOps and Terraform Pipelines/variables.tf
@@ -1,20 +1,3 @@
-variable "bkstrgrg" {
-  type        = string
-  description = "The name of the backend storage account resource group"
-  default     = "<storage act resource group name>"
-}
-
-variable "bkstrg" {
-  type        = string
-  description = "The name of the backend storage account"
-  default     = "<storage account name>"
-}
-
-variable "bkcontainer" {
-  type = string
-  description = "The container name for the backend config"
-  default = "<blob storage container name>"
-}
 
 variable "bkstrgkey" {
   type = string
@@ -34,15 +17,6 @@ variable "location" {
   default     = "<region>"
 }
 
-variable "tags" {
-  type        = map(string)
-  description = "Tags used for the deployment"
-  default = {
-    "Environment" = "Lab"
-    "Owner"       = "<name>"
-  }
-}
-
 variable "vnet_name" {
   type        = string
   description = "The name of the vnet"
@@ -55,31 +29,3 @@ variable "vnet_address_space" {
   default     = ["10.13.0.0/16"]
 }
 
-variable "subnets" {
-  type = map(any)
-  default = {
-    subnet_1 = {
-      name             = "subnet_1"
-      address_prefixes = ["10.13.1.0/24"]
-    }
-    subnet_2 = {
-      name             = "subnet_2"
-      address_prefixes = ["10.13.2.0/24"]
-    }
-    subnet_3 = {
-      name             = "subnet_3"
-      address_prefixes = ["10.13.3.0/24"]
-    }
-    # The name must be AzureBastionSubnet
-    bastion_subnet = {
-      name             = "AzureBastionSubnet"
-      address_prefixes = ["10.13.250.0/24"]
-    }
-  }
-}
-
-variable "bastionhost_name" {
-  type        = string
-  description = "The name of the basion host"
-  default     = "<bastion host name>"
-}
